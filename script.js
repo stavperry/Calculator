95dd970f5f214d63c542f4ffebd3abd04dee2a17
stavperry <162802041+stavperry@users.noreply.github.com>
2026-04-13 22:39:15 +0100

    small HTML reorder of tags

---
+++ script.js
@@ -0,0 +1,8 @@
+// psuedocode:
+// 1. pressing C clears and restarts the screen
+// 2. pressing any of the digits before an operator concats them into a NUMBER.
+// 2.b -> a press on the point makes it a decimal but can only be pressed once per "number"
+// 3. operator chooses which function we run
+// 4. second number added the same as step 2
+// 5. Either a press on the "equal" to display the mathemtical result OR a press on another operator, also calculates.
+// 6. Unless the C is pressed, that calculated number is the new sum (so if we had 2+2 and then we press "x", it's going to be like we're multiplying a "4")
