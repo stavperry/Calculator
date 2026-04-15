6aadd74669b4e8690cb5a7b76f788bce875ca9f6
stavperry <162802041+stavperry@users.noreply.github.com>
2026-04-15 22:43:23 +0100

    adding a CLEAR button

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // psuedocode:
 // 1. pressing C clears and restarts the screen
-// 2. pressing any of the digits before an operator concats them into a NUMBER.
+// 2. pressing any of the digits before an operator concats them into a NUMBER. --> DONE
 // 2.b -> a press on the point makes it a decimal but can only be pressed once per "number"
-// 3. operator chooses which function we run
-// 4. second number added the same as step 2
-// 5. Either a press on the "equal" to display the mathemtical result OR a press on another operator, also calculates.
+// 3. operator chooses which function we run --> DONE
+// 4. second number added the same as step 2 --> DONE
+// 5. Either a press on the "equal" to display the mathemtical result (--> DONE) OR a press on another operator, also calculates.
 // 6. Unless the C is pressed, that calculated number is the new sum (so if we had 2+2 and then we press "x", it's going to be like we're multiplying a "4")
 
 const screen = document.querySelector(".screen");
@@ -65,4 +65,14 @@ calculate.addEventListener("click", (e) =>{
     } else {
         field.textContent = multiply(Number(numA), Number(numB));
     }
+});
+
+const clrBtn = document.querySelector("#clear")
+
+clrBtn.addEventListener("click", (e) => {
+    field.textContent = "";
+    numA = "";
+    numB = "";
+    operatorClicked = false;
+    operatorType = undefined;
 });
